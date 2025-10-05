92ebcf09645fd5e7c9099eb3391637025d1a1c0b
Matheus Figueiredo <alvesmatheusbr100@gmail.com>
2025-10-05 09:32:23 -0300

    Add files via upload

---
+++ Lista_1/Q1/Script1.js
@@ -0,0 +1,16 @@
+function Azul(p) {
+    let elemento = document.getElementById(p);
+    elemento.className = "Azul";
+}   
+function Vermelho(p) {
+    let elemento = document.getElementById(p);
+    elemento.className = "Vermelho";
+}   
+function Verde(p) {
+    let elemento = document.getElementById(p);
+    elemento.className = "Verde";
+}   
+function Amarelo(p) {
+    let elemento = document.getElementById(p);
+    elemento.className = "Amarelo";
+}   
