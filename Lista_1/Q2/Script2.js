19fd3954a3179be329d9954d0480d45d2a27d6ae
Matheus Figueiredo <alvesmatheusbr100@gmail.com>
2025-10-05 09:34:39 -0300

    Add files via upload

---
+++ Lista_1/Q2/Script2.js
@@ -0,0 +1,15 @@
+function Azul(){
+    document.getElementById("a").className="Azul";
+} 
+function Padrao(){
+    document.getElementById("a").className=" ";
+}
+function Roxo(){
+    document.getElementById("a").className="Roxo";
+} 
+function Verde(){
+    document.getElementById("a").className="Verde";
+} 
+function Amarelo(){
+    document.getElementById("a").className="Amarelo";
+} 
